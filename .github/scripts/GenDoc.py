6962e20d6f679640485fd82d62472e0c081442c8
fkaag71 <96815277+fkaag71@users.noreply.github.com>
2026-09-15 14:30:50 +0200

    GenDoc produces text source for Weblate and version
    Version is the day of the latest modification of source objects.

--- .github/scripts/GenDoc.py
+++ .github/scripts/GenDoc.py
@@ -1,7 +1,6 @@
-import yaml,csv,json
-import sys, os, pathlib
-from pathlib import Path
-from datetime import datetime
+import yaml,json
+import sys,pathlib
+
 def loadUnits(type):
     print (f'Loading {type} units')
     files = pathlib.Path(f'Units/{type}').rglob('*.yml')
@@ -12,60 +11,52 @@ def loadUnits(type):
             tempdir[file.stem] = yaml.safe_load(data)
     return dict(sorted(tempdir.items()))
 
-def loadLanguages():
-    files = pathlib.Path(f'Translations').rglob('nuva_*.yml')
-    for file in files:
-        with open(file, encoding='utf-8') as data:
-            langterms = yaml.safe_load(data)
-            lang=next(iter(langterms))
-        for theme in ['disease','valence','vaccine']:
-            for key,term in langterms[lang][theme].items():
-                if key not in Terms:
-                    Terms[key] = {}
-                Terms[key][lang] = term
-
 Codes = {}
-Terms = {}
+Terms = {'en': {'vaccine':{},'valence': {}}}
 
 Vaccines = loadUnits("Vaccines")
 Valences = loadUnits("Valences")
-Targets = loadUnits("Targets")
-CodeSystems = loadUnits("CodeSystems")
 
-loadLanguages()
-
-if (len(sys.argv)>1):
-    version = sys.argv[1]
-else:
-    version = "Unknown"
+version = "2000-01-01"
 
 print("Creating the JSON datafile")
-jsonData = {'version': version,
+jsonData = {'version': None,
             "vaccines": {},
             "valences": {'Valence': {'shorthand': 'VAL', 'label': 'Valence', 'parent': 'Valence'}}
             }
 
 for code,data in Vaccines.items():
+    version = data['modified'] if data['modified']>version else version
+
     if data['abstract']:
+        comment = "" if data['comment']==data['label'] else data['comment']
         jsonData['vaccines'][code] = {
             'abstract' : True,
             'status': data['status'],
             'label': data['label'],
             'created': data['created'],
             'modified': data['modified'],
-            'comment': data['comment'],
+            'comment': comment,
             'valences': data['valences'].copy()
         }
+        Terms['en']['vaccine'][code+'L'] = data['label']
+        if comment != '':
+            Terms['en']['vaccine'][code+'C'] = comment
+
     else:
+        instanceOf = data['instanceOf'] if data['instanceOf'] else 'VAC0000'
+        comment = "" if data['comment']== Vaccines[instanceOf]['label'] else data['comment']
         jsonData['vaccines'][code] = {
             'abstract' : False,
             'status': data['status'],
             'label': data['label'],
             'created': data['created'],
             'modified': data['modified'],
-            'comment': data['comment'],            
-            'instanceOf': data['instanceOf'] or "VAC0000",
+            'comment': comment,
+            'instanceOf': instanceOf
         }
+        if comment != '':
+            Terms['en']['vaccine'][code+'C'] = comment
 
 for code,data in Valences.items():
     jsonData['valences'][code]={
@@ -75,9 +66,16 @@ for code,data in Valences.items():
         'label': data['label'],
         'vtype': data['vtype'],
         'parent': data['parent']}
+    Terms['en']['valence'][code+'S'] = data['shorthand']
+    Terms['en']['valence'][code+'L'] = data['label']
+
+jsonData['version'] = version
 
 with open('docs/data/nuvadata.json','w',encoding='utf-8-sig') as f:
-    f.write(json.dumps(jsonData,ensure_ascii=False, indent = 1))
+    f.write(json.dumps(jsonData,ensure_ascii=False))
+
+with open('Translations/nuva_en.yml','w',encoding='utf-8-sig') as f:
+    yaml.dump(Terms,f)
 
 print("Done")
 
